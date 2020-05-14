4417f6cd0070eb548da2482e262ced0af3217d23
Sweets <sweetmantech@gmail.com>
2020-05-14 12:01:51 -0400

    I add Firebase UI to RegisterDialog

--- src/logged_out/components/register_login/RegisterDialog.js
+++ src/logged_out/components/register_login/RegisterDialog.js
@@ -1,18 +1,26 @@
-import React, { useState, useCallback, useRef, Fragment } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import PropTypes from "prop-types";
 import {
-  FormHelperText,
-  TextField,
-  Button,
-  Checkbox,
-  Typography,
-  FormControlLabel,
   withStyles,
 } from "@material-ui/core";
 import FormDialog from "../../../shared/components/FormDialog";
-import HighlightedInformation from "../../../shared/components/HighlightedInformation";
-import ButtonCircularProgress from "../../../shared/components/ButtonCircularProgress";
-import VisibilityPasswordTextField from "../../../shared/components/VisibilityPasswordTextField";
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+var serviceAccount = require("../../../serviceAccountKey.json");
+
+firebase.initializeApp(serviceAccount);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ]
+};
 
 const styles = (theme) => ({
   link: {
@@ -32,19 +40,12 @@ const styles = (theme) => ({
 });
 
 function RegisterDialog(props) {
-  const { setStatus, theme, onClose, openTermsDialog, status, classes } = props;
+  const { setStatus, onClose } = props;
   const [isLoading, setIsLoading] = useState(false);
-  const [hasTermsOfServiceError, setHasTermsOfServiceError] = useState(false);
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const registerTermsCheckbox = useRef();
   const registerPassword = useRef();
   const registerPasswordRepeat = useRef();
 
   const register = useCallback(() => {
-    if (!registerTermsCheckbox.current.checked) {
-      setHasTermsOfServiceError(true);
-      return;
-    }
     if (
       registerPassword.current.value !== registerPasswordRepeat.current.value
     ) {
@@ -59,10 +60,8 @@ function RegisterDialog(props) {
   }, [
     setIsLoading,
     setStatus,
-    setHasTermsOfServiceError,
     registerPassword,
-    registerPasswordRepeat,
-    registerTermsCheckbox,
+    registerPasswordRepeat
   ]);
 
   return (
@@ -78,158 +77,10 @@ function RegisterDialog(props) {
       hideBackdrop
       hasCloseIcon
       content={
-        <Fragment>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            error={status === "invalidEmail"}
-            label="Email Address"
-            autoFocus
-            autoComplete="off"
-            type="email"
-            onChange={() => {
-              if (status === "invalidEmail") {
-                setStatus(null);
-              }
-            }}
-            FormHelperTextProps={{ error: true }}
-          />
-          <VisibilityPasswordTextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            error={
-              status === "passwordTooShort" || status === "passwordsDontMatch"
-            }
-            label="Password"
-            inputRef={registerPassword}
-            autoComplete="off"
-            onChange={() => {
-              if (
-                status === "passwordTooShort" ||
-                status === "passwordsDontMatch"
-              ) {
-                setStatus(null);
-              }
-            }}
-            helperText={(() => {
-              if (status === "passwordTooShort") {
-                return "Create a password at least 6 characters long.";
-              }
-              if (status === "passwordsDontMatch") {
-                return "Your passwords dont match.";
-              }
-              return null;
-            })()}
-            FormHelperTextProps={{ error: true }}
-            isVisible={isPasswordVisible}
-            onVisibilityChange={setIsPasswordVisible}
-          />
-          <VisibilityPasswordTextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            error={
-              status === "passwordTooShort" || status === "passwordsDontMatch"
-            }
-            label="Repeat Password"
-            inputRef={registerPasswordRepeat}
-            autoComplete="off"
-            onChange={() => {
-              if (
-                status === "passwordTooShort" ||
-                status === "passwordsDontMatch"
-              ) {
-                setStatus(null);
-              }
-            }}
-            helperText={(() => {
-              if (status === "passwordTooShort") {
-                return "Create a password at least 6 characters long.";
-              }
-              if (status === "passwordsDontMatch") {
-                return "Your passwords dont match.";
-              }
-            })()}
-            FormHelperTextProps={{ error: true }}
-            isVisible={isPasswordVisible}
-            onVisibilityChange={setIsPasswordVisible}
-          />
-          <FormControlLabel
-            style={{ marginRight: 0 }}
-            control={
-              <Checkbox
-                color="primary"
-                inputRef={registerTermsCheckbox}
-                onChange={() => {
-                  setHasTermsOfServiceError(false);
-                }}
-              />
-            }
-            label={
-              <Typography variant="body1">
-                I agree to the
-                <span
-                  className={classes.link}
-                  onClick={isLoading ? null : openTermsDialog}
-                  tabIndex={0}
-                  role="button"
-                  onKeyDown={(event) => {
-                    // For screenreaders listen to space and enter events
-                    if (
-                      (!isLoading && event.keyCode === 13) ||
-                      event.keyCode === 32
-                    ) {
-                      openTermsDialog();
-                    }
-                  }}
-                >
-                  {" "}
-                  terms of service
-                </span>
-              </Typography>
-            }
-          />
-          {hasTermsOfServiceError && (
-            <FormHelperText
-              error
-              style={{
-                display: "block",
-                marginTop: theme.spacing(-1),
-              }}
-            >
-              In order to create an account, you have to accept our terms of
-              service.
-            </FormHelperText>
-          )}
-          {status === "accountCreated" ? (
-            <HighlightedInformation>
-              We have created your account. Please click on the link in the
-              email we have sent to you before logging in.
-            </HighlightedInformation>
-          ) : (
-            <HighlightedInformation>
-              Registration is disabled until we go live.
-            </HighlightedInformation>
-          )}
-        </Fragment>
-      }
-      actions={
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          size="large"
-          color="secondary"
-          disabled={isLoading}
-        >
-          Register
-          {isLoading && <ButtonCircularProgress />}
-        </Button>
+        <div>
+          Register your New Voice Dry Cleaner Account.
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+        </div>
       }
     />
   );
