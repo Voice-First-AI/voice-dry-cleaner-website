880e333aab716b65a3c48c748578255c7fc07053
Sweetmantech <sweetmantech@gmail.com>
2020-09-16 21:46:32 -0400

    I remove blog posts from ROUTING

--- src/logged_out/components/Routing.js
+++ src/logged_out/components/Routing.js
@@ -4,25 +4,11 @@ import { Switch } from "react-router-dom";
 import PropsRoute from "../../shared/components/PropsRoute";
 import Home from "./home/Home";
 import Blog from "./blog/Blog";
-import BlogPost from "./blog/BlogPost";
 
 function Routing(props) {
   const { blogPosts, selectBlog, selectHome } = props;
   return (
     <Switch>
-      {blogPosts.map(post => (
-        <PropsRoute
-          /* We cannot use the url here as it contains the get params */
-          path={post.url}
-          component={BlogPost}
-          title={post.title}
-          key={post.title}
-          src={post.imageSrc}
-          date={post.date}
-          content={post.content}
-          otherArticles={blogPosts.filter(blogPost => blogPost.id !== post.id)}
-        />
-      ))}
       <PropsRoute
         exact
         path="/blog"
